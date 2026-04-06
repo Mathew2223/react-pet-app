71d7ad1548973ead969782337169b417b2a58645
Ситниковы <sitnikovmatvey2008@gmail.com>
2026-04-06 09:59:13 +0300

    add global interface

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './routes/root';
 import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-  </StrictMode>,
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
