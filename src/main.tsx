5ae2917a523622d518c4c065da00fc81cbba7f93
Ситниковы <sitnikovmatvey2008@gmail.com>
2026-04-07 21:38:23 +0300

    fix contacts interface

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,8 @@ import {
 import Root, { loader as rootLoader, action as rootAction } from './routes/root';
 import ErrorPage from './error-page';
 import Contact, { loader as contactLoader } from './routes/contacts';
-import EditContact from './routes/edit';
+import EditContact, { action as editAction } from './routes/edit';
+import { action as destroyAction } from './routes/destroy';
 import './index.css'
 
 const router = createBrowserRouter([
@@ -27,6 +28,11 @@ const router = createBrowserRouter([
         path: "contacts/:contactId/edit",
         element: <EditContact />,
         loader: contactLoader,
+        action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction
       }
     ],
   },
